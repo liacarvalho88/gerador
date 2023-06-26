a4c9672280d5b4ff0654edb285b31dea489af202
liacarvalho88 <carvalhonatalia031@gmail.com>
2023-06-26 15:08:17 -0300

    mudancas no gerador no,html,css e javascript.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function fuja() {
-  var botaoNao = document.getElementById("nao")
-
-  var larguraJanela = window.innerwidth;
+  var larguraJanela = window.innerWidth;
   var alturaJanela = window.innerHeight;
   var maxX = larguraJanela - botaoNao.offsetWidth;
-
-  var maxX = alturaJanela - botaoNao.offsetHeight;
+  var maxY = alturaJanela - botaoNao.offsetHeight;
 
   var aleatorioX = Math.floor(Math.random() * maxX);
-  var aleatorioY = Math.floor(Math.random * maxY);
+  var aleatorioY = Math.floor(Math.random() * maxY);
 
   botaoNao.style.left = aleatorioX + "px";
   botaoNao.style.top = aleatorioY + "px";
 }
-
+var botaoNao = document.getElementById("nao");
+botaoNao.addEventListener("mouseover", fuja);
